4255a34ea26031a851a8e00ad7d66ea46df50629
Stepan Kosatyi <stepan@kosatyi.com>
2015-08-10 19:08:52 +0300

    refactor p2pcredit page

--- templates/pages/p2pcredit.php
+++ templates/pages/p2pcredit.php
@@ -1,6 +1,7 @@
 <?
 
 $order_id   = rand(121123213,293929039933);
+
 $result = $ipsp->call('p2pcredit',array(
     'order_id'  => $order_id,
     'order_desc'=> 'Test order description',
@@ -8,7 +9,10 @@ $result = $ipsp->call('p2pcredit',array(
     'amount'    => 200,
     'receiver_card_number'=> 4444555566661111
 ));
+
 $data = $result->getResponse();
 
 if( $data->isSuccess() )
     Flight::redirect($data->checkout_url);
+else
+    Flight::render('fragments/response',array('data'=>$data));
