17196369ad6e938e6e32509728a308ea96678278
Stepan Kosatyi <stepan@kosatyi.com>
2015-08-11 17:46:30 +0300

    refactor project

--- templates/pages/callback.php
+++ templates/pages/callback.php
@@ -1,10 +1,13 @@
 <?
 
     $mongo = new MongoClient;
+
     $db  = $mongo->selectDB('shop');
     $collection  = $db->selectCollection('ipsp_callbacks');
-
     $result = $ipsp->call('result');
 
     if( $result->getResponse() )
+    {
         $collection->insert($result->getResponse());
+    }
+
