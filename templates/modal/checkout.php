07f9d7083252bcd94574464cfab27ef90ee03d6d
Stepan Kosatyi <stepan@kosatyi.com>
2015-08-11 14:28:32 +0300

    fix response url in modal checkout

--- templates/modal/checkout.php
+++ templates/modal/checkout.php
@@ -1,11 +1,15 @@
 <?
-    $result = $ipsp->call('checkout',array(
-        'order_id'  => uniqid('order'),
-        'order_desc'=> 'Test order',
-        'currency'  => $ipsp::USD,
-        'amount'    => 2000
-    ));
-    $data = $result->getResponse();
+$urlpattern = 'http://%s/page/result/%s';
+$host       = $_SERVER['HTTP_HOST'];
+$order_id   = uniqid('order');
+$result = $ipsp->call('checkout',array(
+    'order_id'  => $order_id,
+    'order_desc'=> 'Test order',
+    'currency'  => $ipsp::USD,
+    'amount'    => 2000,
+    'response_url'=>sprintf($urlpattern,$host,$order_id)
+));
+$data = $result->getResponse();
 ?>
 <div class="modal-window modal-checkout">
     <div class="modal-header">
